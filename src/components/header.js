f2ff849fa6e25cdef8471f2c9010186240dfedd9
Alex <alex@gearedapp.co.uk>
2018-11-13 16:56:01 +0000

    test

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,47 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Date } from 'core-js'
+import { runInThisContext } from 'vm'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = { test: 0 }
+  }
+  componentDidMount() {
+    console.log('did mount')
+    this.setState({ test: Math.random() })
+  }
+  render() {
+    const { siteTitle } = this.props
+    console.log('state', this.state.test)
+    return (
+      <div
+        style={{
+          background: 'rebeccapurple',
+          marginBottom: '1.45rem',
+        }}
+      >
+        <div
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '1.45rem 1.0875rem',
           }}
         >
-          {siteTitle}
-        </Link>
-        <div
-          key={Math.random()}
-          dangerouslySetInnerHTML={{
-            __html: `
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              {siteTitle}
+            </Link>
+
+            <div
+              dangerouslySetInnerHTML={{
+                __html: `
               <div id="google_translate_element"></div><script type="text/javascript">
               function googleTranslateElementInit() {
                 new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
@@ -37,11 +49,54 @@ const Header = ({ siteTitle }) => (
               console.log('triggered');
               </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
           `,
-          }}
-        />
-      </h1>
-    </div>
-  </div>
-)
+              }}
+            />
+          </h1>
+        </div>
+      </div>
+    )
+  }
+}
+// const Header = ({ siteTitle }) => (
+//   <div
+//     style={{
+//       background: 'rebeccapurple',
+//       marginBottom: '1.45rem',
+//     }}
+//   >
+//     <div
+//       style={{
+//         margin: '0 auto',
+//         maxWidth: 960,
+//         padding: '1.45rem 1.0875rem',
+//       }}
+//     >
+//       <h1 style={{ margin: 0 }}>
+//         <Link
+//           to="/"
+//           style={{
+//             color: 'white',
+//             textDecoration: 'none',
+//           }}
+//         >
+//           {siteTitle}
+//         </Link>
+
+//         <div
+//           dangerouslySetInnerHTML={{
+//             __html: `
+//               <div id="google_translate_element"></div><script type="text/javascript">
+//               function googleTranslateElementInit() {
+//                 new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+//               }
+//               console.log('triggered');
+//               </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+//           `,
+//           }}
+//         />
+//       </h1>
+//     </div>
+//   </div>
+// )
 
 export default Header
