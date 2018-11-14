5151fd907d9ffd7179c9be30f783421da2c09feb
Alex <alex@gearedapp.co.uk>
2018-11-14 10:15:39 +0000

    test

--- src/components/googleTranslate.js
+++ src/components/googleTranslate.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 
 const wait = time => new Promise((resolve, reject) => setTimeout(resolve, time))
+const windowGlobal = typeof window !== 'undefined' && window
 
 const googleTranslateElementInit = () => {
-  console.log('google loaded', window.google)
-  new window.google.translate.TranslateElement(
+  console.log('google loaded', windowGlobal.google)
+  new windowGlobal.google.translate.TranslateElement(
     { pageLanguage: 'en' },
     'google_translate_element'
   )
 }
-window.googleTranslateElementInit = googleTranslateElementInit
+windowGlobal.googleTranslateElementInit = googleTranslateElementInit
 
 export default class GoogleTranslate extends Component {
   constructor() {
@@ -30,12 +31,9 @@ export default class GoogleTranslate extends Component {
     return (
       <>
         <div id="google_translate_element" />
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `
-          <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-          `,
-          }}
+        <script
+          type="text/javascript"
+          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
         />
       </>
     )
