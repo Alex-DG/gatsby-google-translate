4676617ef88d51c4dbbd26c5096190c78692149e
Alex <alex@gearedapp.co.uk>
2018-11-14 12:31:42 +0000

    test

--- src/components/googleTranslate.js
+++ src/components/googleTranslate.js
@@ -5,9 +5,14 @@ export default class GoogleTranslate extends Component {
     // console.log('clearing google translate div')
     // document.querySelector('#google_translate_element').innerHTML = ''
     // console.log('cleared')
+
     window.googleTranslateElementInit = this.googleTranslateElementInit
 
+    const scriptLoaded = document.querySelector('#gtranslate-script')
+    if (scriptLoaded) scriptLoaded.remove()
+
     const googleTranslateScript = document.createElement('script')
+    googleTranslateScript.id = 'gtranslate-script'
     googleTranslateScript.type = 'text/javascript'
     googleTranslateScript.async = true
     googleTranslateScript.src =
@@ -16,8 +21,6 @@ export default class GoogleTranslate extends Component {
       document.getElementsByTagName('head')[0] ||
       document.getElementsByTagName('body')[0]
     ).appendChild(googleTranslateScript)
-
-    console.log(googleTranslateScript)
   }
 
   googleTranslateElementInit() {
@@ -31,6 +34,14 @@ export default class GoogleTranslate extends Component {
   }
 
   render() {
-    return <div id="google_translate_element" />
+    return (
+      <>
+        <div id="google_translate_element" />
+        {/* <script
+          type="text/javascript"
+          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        /> */}
+      </>
+    )
   }
 }
