5eb29b5a67b848b1558771c83654722564998317
Alex <alex@gearedapp.co.uk>
2018-11-14 11:07:56 +0000

    test

--- src/components/googleTranslate.js
+++ src/components/googleTranslate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet'
 
 export default class GoogleTranslate extends Component {
   componentDidMount() {
@@ -7,17 +8,17 @@ export default class GoogleTranslate extends Component {
     // console.log('cleared')
     window.googleTranslateElementInit = this.googleTranslateElementInit
 
-    const googleTranslateScript = document.createElement('script')
-    googleTranslateScript.type = 'text/javascript'
-    googleTranslateScript.async = true
-    googleTranslateScript.src =
-      '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
-    ;(
-      document.getElementsByTagName('head')[0] ||
-      document.getElementsByTagName('body')[0]
-    ).appendChild(googleTranslateScript)
+    // const googleTranslateScript = document.createElement('script')
+    // googleTranslateScript.type = 'text/javascript'
+    // googleTranslateScript.async = true
+    // googleTranslateScript.src =
+    //   '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
+    // ;(
+    //   document.getElementsByTagName('head')[0] ||
+    //   document.getElementsByTagName('body')[0]
+    // ).appendChild(googleTranslateScript)
 
-    console.log(googleTranslateScript)
+    // console.log(googleTranslateScript)
   }
 
   googleTranslateElementInit() {
@@ -31,6 +32,16 @@ export default class GoogleTranslate extends Component {
   }
 
   render() {
-    return <div id="google_translate_element" />
+    return (
+      <>
+        <div id="google_translate_element" />
+        <Helmet>
+          <script
+            type="text/javascript"
+            src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+          />
+        </Helmet>
+      </>
+    )
   }
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -27,10 +27,6 @@ const Layout = ({ children }) => (
           ]}
         >
           <html lang="en" />
-          {/* <script
-            type="text/javascript"
-            src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-          /> */}
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
