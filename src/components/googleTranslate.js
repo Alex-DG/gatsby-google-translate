0a7fad73d7d40f9f12272891fdeb11dd771b4c99
Alex <alex@gearedapp.co.uk>
2018-11-14 11:02:42 +0000

    test

--- src/components/googleTranslate.js
+++ src/components/googleTranslate.js
@@ -11,7 +11,7 @@ export default class GoogleTranslate extends Component {
     googleTranslateScript.type = 'text/javascript'
     googleTranslateScript.async = true
     googleTranslateScript.src =
-      'http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
+      '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
     ;(
       document.getElementsByTagName('head')[0] ||
       document.getElementsByTagName('body')[0]
