24be67ec5dfaded576b8b8529ee0c075f39646fa
Alex <alex@gearedapp.co.uk>
2018-11-14 10:44:02 +0000

    test

--- src/components/googleTranslate.js
+++ src/components/googleTranslate.js
@@ -6,47 +6,29 @@ export default class GoogleTranslate extends Component {
     this.state = {}
   }
   componentDidMount() {
-    if (
-      document.body.contains(
-        document.querySelector('#google_translate_element')
-      )
-    ) {
-      window.google && window.googleTranslateElementInit()
-    }
-
-    // const googleTranslateElementInit = () => {
-    //   console.log('google loaded', window.google)
-    //   new window.google.translate.TranslateElement(
-    //     { pageLanguage: 'en' },
-    //     'google_translate_element'
-    //   )
-    // }
-    // window.googleTranslateElementInit = googleTranslateElementInit
+    console.log('clearing google translate div')
+    document.querySelector('#google_translate_element').innerHTML = ''
+    console.log('cleared')
   }
 
   render() {
     return (
-      // <>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-        <div id="google_translate_element"></div><script type="text/javascript">
-function googleTranslateElementInit() {
-  console.log('google translate is loading')
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-  console.log('google translate has loaded')
-}
-</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
-        `,
-        }}
-      />
-      // <div id="google_translate_element" />
-      // <script
-      //   type="text/javascript"
-      //   src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-      // />
-      // </>
+      <>
+        <div id="google_translate_element" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+          function googleTranslateElementInit() {
+            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+          }`,
+          }}
+        />
+        <script
+          type="text/javascript"
+          src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+        />
+      </>
     )
   }
 }
