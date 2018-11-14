1e9723d39c8e6db62bb047992896900c4f39b15d
Alex <alex@gearedapp.co.uk>
2018-11-14 11:14:05 +0000

    test

--- src/components/googleTranslate.js
+++ src/components/googleTranslate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Helmet from 'react-helmet'
 
 export default class GoogleTranslate extends Component {
   componentDidMount() {
@@ -7,17 +8,25 @@ export default class GoogleTranslate extends Component {
     // console.log('cleared')
     window.googleTranslateElementInit = this.googleTranslateElementInit
 
-    const googleTranslateScript = document.createElement('script')
-    googleTranslateScript.type = 'text/javascript'
-    googleTranslateScript.async = true
-    googleTranslateScript.src =
-      '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
-    ;(
-      document.getElementsByTagName('head')[0] ||
-      document.getElementsByTagName('body')[0]
-    ).appendChild(googleTranslateScript)
+    const languageSelector = document.querySelector(
+      '#google_translate_element #:0.targetLanguage'
+    )
+    if (window.google && !document.body.contains(languageSelector)) {
+      console.log('manually loading google translate')
+      this.googleTranslateElementInit()
+    }
 
-    console.log(googleTranslateScript)
+    // const googleTranslateScript = document.createElement('script')
+    // googleTranslateScript.type = 'text/javascript'
+    // googleTranslateScript.async = true
+    // googleTranslateScript.src =
+    //   '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
+    // ;(
+    //   document.getElementsByTagName('head')[0] ||
+    //   document.getElementsByTagName('body')[0]
+    // ).appendChild(googleTranslateScript)
+
+    // console.log(googleTranslateScript)
   }
 
   googleTranslateElementInit() {
@@ -31,6 +40,16 @@ export default class GoogleTranslate extends Component {
   }
 
   render() {
-    return <div id="google_translate_element" />
+    return (
+      <>
+        <div id="google_translate_element" />
+        <Helmet>
+          <script
+            type="text/javascript"
+            src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+          />
+        </Helmet>
+      </>
+    )
   }
 }
